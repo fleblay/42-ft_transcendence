4c16b19d99ea8ce4d5fd389b0ab326bd3fdce569
jremy <jonathan.prudent13@gmail.com>
2023-03-28 14:48:43 +0200

    fix request compilation

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,10 +1,9 @@
-import { Get, Body, Controller, UseGuards } from '@nestjs/common';
+import { Get, Body, Controller, UseGuards, Request} from '@nestjs/common';
 import { Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
-import { Request } from 'express';
 import { AuthenticatedGuard } from './authenticated.guard';
 
 @Controller('users')
