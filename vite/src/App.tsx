0fdc94601062cf8b8513fc8eaf1cbe80e80df1ec
Hoppy <mbraets@student.42.fr>
2023-03-30 16:29:34 +0200

    Restructure router and routes with protected one

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -4,7 +4,9 @@ import { GameCanvas } from './game/game'
 import React, { createContext, useState } from 'react'
 import { Socket, io } from 'socket.io-client'
 import { getToken } from './token/token'
-import * as Router from 'react-router-dom'
+import { LoginForm } from './pages/LoginPage'
+import { Link, Navigate, Outlet, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { fakeAuthProvider } from './auth'
 
 /*
 const socket = new WebSocket(`ws://${window.location.host}/ws/`)
@@ -28,9 +30,162 @@ socket.onopen = function () {
 };
 */
 
+interface AuthContextType {
+	user: any;
+	login: (user: string, callback: VoidFunction) => void;
+	logout: (callback: VoidFunction) => void;
+}
+
+let AuthContext = React.createContext<AuthContextType>(null!);
+
+function AuthService({ children }: { children: React.ReactNode }) {
+	let [user, setUser] = React.useState<any>(null);
+
+	let login = (newUser: string, callback: VoidFunction) => {
+		return fakeAuthProvider.login(() => {
+			setUser(newUser);
+			callback();
+		});
+	};
+
+	let logout = (callback: VoidFunction) => {
+		return fakeAuthProvider.logout(() => {
+			setUser(null);
+			callback();
+		});
+	};
+
+	let value = { user, login, logout };
+
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+function useAuthService() {
+	return React.useContext(AuthContext);
+}
+function AuthStatus() {
+	let auth = useAuthService();
+	let navigate = useNavigate();
+
+	if (!auth.user) {
+		return <p>You are not logged in.</p>;
+	}
+
+	return (
+		<p>
+			Welcome {auth.user}!{" "}
+			<button
+				onClick={() => {
+					auth.logout(() => navigate("/"));
+				}}
+			>
+				Sign out
+			</button>
+		</p>
+	);
+}
+
+function LoginPage() {
+	let navigate = useNavigate();
+	let location = useLocation();
+	let auth = useAuthService();
+
+	let from = location.state?.from?.pathname || "/";
+
+	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		let formData = new FormData(event.currentTarget);
+		let username = formData.get("username") as string;
+
+		auth.login(username, () => {
+			// Send them back to the page they tried to visit when they were
+			// redirected to the login page. Use { replace: true } so we don't create
+			// another entry in the history stack for the login page.  This means that
+			// when they get to the protected page and click the back button, they
+			// won't end up back on the login page, which is also really nice for the
+			// user experience.
+			navigate(from, { replace: true });
+		});
+	}
+
+	return (
+		<div>
+			<p>You must log in to view the page at {from}</p>
+
+			<form onSubmit={handleSubmit}>
+				<label>
+					Username: <input name="username" type="text" />
+				</label>{" "}
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	);
+}
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+	let auth = useAuthService();
+	let location = useLocation();
+
+	if (!auth.user) {
+		// Redirect them to the /login page, but save the current location they were
+		// trying to go to when they were redirected. This allows us to send them
+		// along to that page after they login, which is a nicer user experience
+		// than dropping them off on the home page.
+		return <Navigate to="/login" state={{ from: location }} replace />;
+	}
+
+	return children;
+}
+
+function Header() {
+	return (
+		<div>
+			<AuthStatus />
+
+			<ul>
+				<li>
+					<Link to="/">Game Page</Link>
+				</li>
+				<li>
+					<Link to="/chat">Chat Page</Link>
+				</li>
+			</ul>
+
+			<Outlet />
+		</div>
+	);
+}
+
 export const AppContext = createContext<any>({});
 
 function App() {
+
+	return (
+		<AuthService>
+			<Routes>
+				<Route element={<Header />}>
+					<Route path="/" element={
+						<RequireAuth>
+							<div>Game</div>
+						</RequireAuth>
+					} />
+					<Route path='/public' element={<div>Public</div>} />
+					<Route
+						path="/chat"
+						element={
+							<RequireAuth>
+								<>
+								<label>w</label>
+								<LoginForm />
+								</>
+							</RequireAuth>
+						}
+					/>
+					<Route path="/login" element={<LoginPage />} />
+				</Route>
+			</Routes>
+		</AuthService>
+	);
 	function newSocket() {
 		return io({
 			auth: {
@@ -45,7 +200,7 @@ function App() {
 	}
 
 	const [socket, setSocket] = useState<Socket>(newSocket());
-	const navigate = Router.useNavigate()
+	const navigate = useNavigate()
 	React.useEffect(() => {
 		console.log('Creating event listener');
 		function onConnect() {
@@ -86,7 +241,7 @@ function App() {
 				<GetAll />
 				<GameCanvas />
 				<button onClick={() => { socket.emit('ping', 'Coucou') }}>Say hello to everyone</button>
-				<Router.Link to="/login"> go login</Router.Link>
+				<Link to="/login"> go login</Link>
 			</AppContext.Provider>
 		</div>
 	)
--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -3,33 +3,62 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import {
-	createBrowserRouter,
-	Link,
-	RouterProvider,
-  } from "react-router-dom";
-import { LoginForm } from './pages/LoginPage';
-import { MyForm } from './pages/NameSelector';
-import { GetAll } from './pages/getall';
-import { RegisterForm } from './pages/RegisterPage';
-
-const router = createBrowserRouter([
-	{
-	  path: "/",
-	  element : <App />,
-	},
-	{
-		path: '/login',
-		element: <LoginForm />
-	},
-	{
-		path: '/register',
-		element: <RegisterForm />
-	},
-  ]);
-
+	BrowserRouter,
+	Routes,
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-	<RouterProvider router={router} />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
 )
+
+/*
+type AuthState = 'loading' | 'authenticated' | 'unauthenticated';
+
+function PrivateRoute({ children, ...rest }: any) {
+	const [isAuthenticated, setIsAuthenticated] = React.useState<AuthState>('loading');
+
+	fetch('/api/auth/me')
+		.then(response => setIsAuthenticated('authenticated'))
+		.catch(() => setIsAuthenticated('unauthenticated'));
+
+	function getAction(): JSX.Element {
+		if (isAuthenticated === 'loading') {
+			return <div>Loading...</div>;
+		} else if (isAuthenticated === 'authenticated') {
+			return children;
+		}
+		return <Redirect
+			to={{
+				pathname: '/login',
+				state: { from: rest.location },
+			}}
+		/>
+
+	}
+	return getAction();
+}
+
+function App() {
+	return (
+		<Routes>
+			<PrivateRoute exact path="/">
+				<HomePage />
+			</PrivateRoute>
+			<PrivateRoute path="/game">
+				<GamePage />
+			</PrivateRoute>
+			<PrivateRoute path="/stats">
+				<StatsPage />
+			</PrivateRoute>
+			<Route path="/login">
+				<LoginPage />
+			</Route>
+			<Redirect to="/" />
+		</Routes>
+	);
+}
+*/
