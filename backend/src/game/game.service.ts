f2fea48c57efaea1c07296f968338f713b934590
Hoppy <mbraets@student.42.fr>
2023-04-13 13:40:26 +0200

    Get list of saved games

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -77,4 +77,14 @@ export class GameService {
 			return "null"
 	}
 
+	getListGames(page: number) {
+		return this.repo.find({
+			order: {
+				date: 'DESC'
+			},
+			take: 10,
+			skip: page * 10
+		})
+	}
+
 }
