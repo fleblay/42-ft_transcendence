7156b93de619b83ce3af802f99beb516d489381e
Frederic Le Blay <frederic.leblay@gmail.com>
2023-04-18 16:58:46 +0200

    Refacto front Userlist

--- vite/src/component/ListUsers.tsx
+++ vite/src/component/ListUsers.tsx
@@ -1,4 +1,4 @@
-import { useState} from "react";
+import { useState } from "react";
 import apiClient from '../auth/interceptor.axios'
 
 type Games = {
@@ -8,25 +8,23 @@ type Games = {
 }
 
 type User = {
-	avatar: string,
-	email: string,
-	gameIds: string[],
 	id: number,
 	username: string,
+	email: string,
+	password?: string, // To be removed in DTO in back
+	avatar: string,
+	savedGames: Games[],
+	wonGames: Games[],
+
 	states: string[],
-	userConnected: boolean
-	savedGames: Games[]
-	wonGames: Games[]
+	gameIds: string[],
 
-	totalScore: number
-	password?: string
+	points: number,
+	totalwonGames: number,
+	totalplayedGames: number,
+
+	userConnected: boolean,
 }
-
-type UserState = {
-  states : string[],
-  gameIds : string[],
-}
-
 
 export function ListUsers() {
 
@@ -34,45 +32,26 @@ export function ListUsers() {
 	const [userList, setUserList] = useState<JSX.Element[]>([])
 
 	function handleClick(): void {
-			apiClient
+		apiClient
 			.get("/api/users/all")
-			.then((response) => {
-				console.log("ressponse from all: ", response)
-				setInfo("Successfully retrieve infos :")
-				let partialUserList = response.data.map((elem : User) => {
-					let {password, ...userInfo} = elem
-					return userInfo
-					})
-				return(partialUserList)
-				})
-			.then(async (partialUserList: User[]) => {
-				let partialUserList2 : User[] = await Promise.all(partialUserList.map(async (e: User) => {
-					const userStatus = (await apiClient .get(`/api/game/userstate/${e.id}`)).data as UserState
-					const userConnected = (await apiClient .get(`/api/users/connected/${e.id}`)).data as boolean
-					const totalScore = e.wonGames.reduce((acc, curr) => {
-						return acc + Math.max(...curr.score)
-						}, 0)
-					console.log("This is connected status", userConnected)
-					return {...e, ...userStatus, userConnected, totalScore}
-					}))
-				return(partialUserList2)
-				})
-			.then((partialUserList2) =>{
-				setUserList(partialUserList2.map((elem) =>{
+			.then(({ data }) => {
+				console.log("response from all: ", data)
+				setInfo("Successfully retrieved infos :")
+				setUserList(data.map((elem: User) => {
 					return (
 						<li>{elem.id}
-							<ul style={{listStyleType: "none"}}>
-							<li>{elem.username}</li>
-							<li>{elem.email}</li>
-							<li>Total score : {elem.totalScore}</li>
-							<li>{(elem.states.join('-') == "" ? "No status": elem.states.join('-'))}</li>
-							<li>{(elem.gameIds.join('-') == "" ? "No game": elem.gameIds.join('-'))}</li>
-							<li>{(elem.userConnected) ? "En ligne": "Offline"}</li>
+							<ul style={{ listStyleType: "none" }}>
+								<li>{elem.username}</li>
+								<li>{elem.email}</li>
+								<li>Total score : {elem.points}</li>
+								<li>{(elem.states.join('-') == "" ? "No status" : elem.states.join('-'))}</li>
+								<li>{(elem.gameIds.join('-') == "" ? "No game" : elem.gameIds.join('-'))}</li>
+								<li>{(elem.userConnected) ? "En ligne" : "Offline"}</li>
 							</ul>
 						</li>
 					)
-					}))
-				})
+				}))
+			})
 			.catch((error) => {
 				console.log(error)
 				if (error?.response?.status === 502)
@@ -86,7 +65,7 @@ export function ListUsers() {
 		<div>
 			<button onClick={handleClick}>Update Users list</button>
 			<p>{info}</p>
-			<ul style={{listStyleType: "none"}}>
+			<ul style={{ listStyleType: "none" }}>
 				{...userList}
 			</ul>
 		</div>
