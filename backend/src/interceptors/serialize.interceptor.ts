b52f007e64bb5a36650dc17ff9d8dd1336ef9d48
Hoppy <mbraets@student.42.fr>
2023-04-20 12:16:05 +0200

    Exclude only password

--- backend/src/interceptors/serialize.interceptor.ts
+++ backend/src/interceptors/serialize.interceptor.ts
@@ -16,7 +16,7 @@ export class SerializeInterceptor implements NestInterceptor {
 	intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
 		return handler.handle().pipe(
 			map((data: any) => {
-				return plainToInstance(this.dto, data, { excludeExtraneousValues: true });
+				return plainToInstance(this.dto, data, { excludeExtraneousValues: false });
 			})
 		);
 	}
--- backend/src/users/dtos/user.dto.ts
+++ backend/src/users/dtos/user.dto.ts
@@ -1,14 +1,6 @@
 import { Exclude, Expose } from "class-transformer";
 
 export class UserDto {
-	@Expose()
-	id: number;
-
-	@Expose()
-	username: string;
-
-	@Expose()
-	email: string;
 
 	@Exclude()
 	password: string;
