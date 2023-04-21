911fa863d9b3f7430019f410581c631859997602
Frederic Le Blay <frederic.leblay@gmail.com>
2023-04-21 18:00:32 +0200

    small improvement front

--- vite/src/component/GameScreen.tsx
+++ vite/src/component/GameScreen.tsx
@@ -96,7 +96,8 @@ export function GamePage() {
 								width: '100%',
 								height: '100%',
 								backgroundColor: 'rgba(0, 0, 0, 0.5)',
-								zIndex: 1
+								zIndex: 1,
+								transition: 'all 0.5s'
 							}
 						}>
 							<div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', fontSize: '50px', color: 'white' }}>
