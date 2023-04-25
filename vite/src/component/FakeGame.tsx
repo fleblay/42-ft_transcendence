382314b86fbb478cd42c1f3291d4cd87497b3e70
Frederic Le Blay <frederic.leblay@gmail.com>
2023-04-25 16:51:05 +0200

    Improved leaderboard

--- vite/src/component/FakeGame.tsx
+++ vite/src/component/FakeGame.tsx
@@ -1,21 +1,21 @@
-import { useState} from "react";
+import { useState } from "react";
 import apiClient from '../auth/interceptor.axios'
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
 export function FakeGames() {
 	const [info, setInfo] = useState<string>("No info yet")
 
-	async function handleClick() : Promise<void> {
+	async function handleClick(): Promise<void> {
 		setInfo("Waiting for backend to generate fake games...")
 		let fakeArray = []
-		for(let i = 0; i < 10; i++) {
+		for (let i = 0; i < 10; i++) {
 			fakeArray.push(
-			apiClient.get("/api/game/fake")
+				apiClient.get("/api/game/fake")
 			)
 		}
 		try {
-		await Promise.all(fakeArray)
-		setInfo("Successfully added 10 fake games to history !")
+			await Promise.all(fakeArray)
+			setInfo("Successfully added 10 fake games to history !")
 		} catch (e) {
 			console.log(e)
 			setInfo("Error. More info in console")
@@ -23,12 +23,14 @@ export function FakeGames() {
 	}
 
 	return (
-	<div>
-			<Button variant='contained' onClick={handleClick}>
-				Generate 10 fake games
-			</Button>
-		<p>{info}</p>
-	</div>
+		<div>
+			<Typography textAlign="center">
+				<Button variant='contained' onClick={handleClick}>
+					Generate 10 fake games
+				</Button>
+				<p>{info}</p>
+			</Typography>
+		</div>
 	)
 }
 
