445d3bf3c3d4814189df8294ced1eb11ea84bd0b
Frederic Le Blay <frederic.leblay@gmail.com>
2023-04-25 17:15:33 +0200

    changes leaderboard

--- vite/src/component/ListUsers.tsx
+++ vite/src/component/ListUsers.tsx
@@ -37,59 +37,59 @@ export function ListUsers() {
 				//Mui elements
 				setMuiTable(
 					<Container maxWidth="lg" >
-							<AppBar position="static" sx={{ borderTopLeftRadius: '16px', borderTopRightRadius: '16px', height: '80px' }}>
-								<Typography textAlign="center" variant="h6" sx={{ flexGrow: 1, paddingTop: '25px' }}>
-									Leaderboard
-								</Typography>
-							</AppBar>
-							<TableContainer component={Paper}>
-								<Table sx={{ minWidth: 650 }} aria-label="simple table">
-									<TableHead>
-										<TableRow>
-											<TableCell>Rank</TableCell>
-											<TableCell align="right">Username</TableCell>
-											<TableCell align="right">Points</TableCell>
-											<TableCell align="right">Win/Lose Ratio</TableCell>
-											<TableCell align="right">Won Games</TableCell>
-											<TableCell align="right">Played Games</TableCell>
-											<TableCell align="right">UserId</TableCell>
-											<TableCell align="right">Student?</TableCell>
-											<TableCell align="right">Online?</TableCell>
-											<TableCell align="right">Game Status</TableCell>
-											<TableCell align="right">GameId</TableCell>
+						<AppBar position="static" sx={{ borderTopLeftRadius: '16px', borderTopRightRadius: '16px', height: '80px' }}>
+							<Typography textAlign="center" variant="h6" sx={{ flexGrow: 1, paddingTop: '25px' }}>
+								Leaderboard
+							</Typography>
+						</AppBar>
+						<TableContainer component={Paper}>
+							<Table sx={{ minWidth: 650 }} aria-label="simple table">
+								<TableHead>
+									<TableRow>
+										<TableCell>Rank</TableCell>
+										<TableCell align="right">Username</TableCell>
+										<TableCell align="right">Points</TableCell>
+										<TableCell align="right">Win/Lose Ratio</TableCell>
+										<TableCell align="right">Won Games</TableCell>
+										<TableCell align="right">Played Games</TableCell>
+										<TableCell align="right">UserId</TableCell>
+										<TableCell align="right">Student?</TableCell>
+										<TableCell align="right">Online?</TableCell>
+										<TableCell align="right">Game Status</TableCell>
+										<TableCell align="right">GameId</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{data.map((elem: UserInfo) => (
+										<TableRow
+											key={elem.username}
+											sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+										>
+											<TableCell component="th" scope="row">#{rank++}</TableCell>
+											<TableCell align="right">
+												<Link key={elem.id} component={LinkRouter} to={`/player/${elem.id}`}>{elem.username}</Link>
+											</TableCell>
+											<TableCell align="right">{elem.points}</TableCell>
+											<TableCell align="right">{elem.totalplayedGames ? (elem.totalwonGames / elem.totalplayedGames).toPrecision(2) : 0}</TableCell>
+											<TableCell align="right">{elem.totalwonGames}</TableCell>
+											<TableCell align="right">{elem.totalplayedGames}</TableCell>
+											<TableCell align="right">{elem.id}</TableCell>
+											<TableCell align="right">{elem.stud ? "Yes" : "No"}</TableCell>
+											<TableCell align="right">
+												<Avatar sx={{ bgcolor: elem.userConnected ? 'green' : 'red' }} style={{ width: '15px', height: '15px' }}> </Avatar>
+											</TableCell>
+											<TableCell align="right">{(elem.states.join('-') == "") ? "none" : elem.states.join('-')}</TableCell>
+											<TableCell align="right">{(elem.gameIds.join('-') == "") ? "---" : elem.gameIds.join('-')}</TableCell>
 										</TableRow>
-									</TableHead>
-									<TableBody>
-										{data.map((elem: UserInfo) => (
-											<TableRow
-												key={elem.username}
-												sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-											>
-												<TableCell component="th" scope="row">#{rank++}</TableCell>
-												<TableCell align="right">
-													<Link key={elem.id} component={LinkRouter} to={`/player/${elem.id}`}>{elem.username}</Link>
-												</TableCell>
-												<TableCell align="right">{elem.points}</TableCell>
-												<TableCell align="right">{elem.totalplayedGames ? (elem.totalwonGames / elem.totalplayedGames).toPrecision(2) : 0}</TableCell>
-												<TableCell align="right">{elem.totalwonGames}</TableCell>
-												<TableCell align="right">{elem.totalplayedGames}</TableCell>
-												<TableCell align="right">{elem.id}</TableCell>
-												<TableCell align="right">{elem.stud ? "Yes" : "No"}</TableCell>
-												<TableCell align="right">
-													<Avatar sx={{ bgcolor: elem.userConnected ? 'green' : 'red' }} style={{ width: '15px', height: '15px' }}> </Avatar>
-												</TableCell>
-												<TableCell align="right">{(elem.states.join('-') == "") ? "none" : elem.states.join('-')}</TableCell>
-												<TableCell align="right">{(elem.gameIds.join('-') == "") ? "---" : elem.gameIds.join('-')}</TableCell>
-											</TableRow>
-										))}
-									</TableBody>
-								</Table>
-								<Typography textAlign="center">
-									<Button sx = {{ margin: '10px' }}variant='contained' onClick={() => handleClick(true)}>
-										Manually Update Users list
-									</Button>
-								</Typography>
-							</TableContainer>
+									))}
+								</TableBody>
+							</Table>
+							<Typography textAlign="center">
+								<Button sx={{ margin: '10px' }} variant='contained' onClick={() => handleClick(true)}>
+									Manually Update Users list
+								</Button>
+							</Typography>
+						</TableContainer>
 					</Container>
 				)
 			})
@@ -106,7 +106,7 @@ export function ListUsers() {
 		<div>
 			{muiTable}
 			<Typography textAlign="center">
-			<p>{info}</p>
+				{info}
 			</Typography>
 		</div>
 	);
