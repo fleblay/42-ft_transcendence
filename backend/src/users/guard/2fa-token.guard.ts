ab1822d7594b8a0d3919b25410e6ba1d80929505
Hoppy <mbraets@student.42.fr>
2023-04-28 14:35:56 +0200

    Move 42ValidToken to auth service

--- backend/src/users/guard/2fa-token.guard.ts
+++ backend/src/users/guard/2fa-token.guard.ts
@@ -1,41 +1,28 @@
 import {
 	CanActivate,
 	ExecutionContext,
-    Injectable
+	Injectable,
+	UnauthorizedException
 } from '@nestjs/common';
 import { AuthService } from '../auth/auth.service';
 import { Request } from 'express';
-import { HttpException } from '@nestjs/common';
 import jwt_decode from "jwt-decode";
-import {User} from '../../model/user.entity'
 
 
 @Injectable()
 export class DfaGuard implements CanActivate {
 
-    constructor(private authService: AuthService) {}
+	constructor(private authService: AuthService) { }
 
-	async canActivate(context: ExecutionContext){
+	async canActivate(context: ExecutionContext) {
 		const request = context.switchToHttp().getRequest() as Request;
-		//console.log("ATGuard: before replace", request.headers.authorization);
-        const bearerToken = request.headers.authorization?.replace(/Bearer ?/, '');
-		//console.log("ATGuard: after replace", request.headers.authorization);
-		//console.log ("bearer token", bearerToken)
-		if (!bearerToken) {
-			console.log("ATGuard: no bearer token");
+		const dfaToken = request.cookies['dfa_token'];
+		if (!dfaToken) {
 			return false;
 		}
-		let decoded = jwt_decode(bearerToken) as any;
+		let decoded = jwt_decode(dfaToken) as any;
 		if (decoded.exp < Date.now() / 1000)
-			throw new HttpException('Token invalide', 498);
-        const user = await this.authService.validateAccessToken(bearerToken);
-		//console.log("ATGuard: user", user);
-		if (!user || user === undefined) {
-			console.log("ATGuard: no user");
-			return false;
-		}
-		(request as Request & {currentUser: User}).currentUser  = user;
+			throw new UnauthorizedException('DFA Token expired');
 		return true;
 	}
 }
-//
