50352593d33cf8f3a6668e38ebcba0f6d1a061f8
Hoppy <mbraets@student.42.fr>
2023-05-02 15:57:57 +0200

    createMessage done

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, forwardRef } from '@nestjs/common';
 import { Member } from '../model/member.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,11 +7,12 @@ import { Message } from '../model/message.entity';
 import { UsersService } from '../users/users.service';
 import { Server } from 'socket.io'
 import { NewMessageDto } from './dto/new-message.dto';
+import { User } from '../model/user.entity';
 @Injectable()
 export class ChatService {
 
 	private wsServer: Server;
-	constructor (
+	constructor(
 		@InjectRepository(Member) private membersRepo: Repository<Member>,
 		@InjectRepository(Channel) private channelsRepo: Repository<Channel>,
 		@InjectRepository(Message) private messagesRepo: Repository<Message>,
@@ -22,7 +23,17 @@ export class ChatService {
 		this.wsServer = server;
 	}
 
-	newMessage(channelId: number, messageData: NewMessageDto) {
+	async newMessage(owner: User, channelId: number, messageData: NewMessageDto) {
+		const channel = await this.channelsRepo.findOneBy({ id: channelId });
+		if (!channel)
+			throw new NotFoundException('Channel not found');
+		const newMessage = this.messagesRepo.create({
+			channel,
+			owner,
+			gameId: messageData.gameId,
+			content: messageData.content
+		});
+		await this.messagesRepo.save(newMessage);
 		this.wsServer.to(`/chat/${channelId}`).emit('chat.newMessage', messageData);
 	}
 }
