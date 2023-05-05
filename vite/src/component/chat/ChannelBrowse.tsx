6a04f15ae93aed6deceae4b4f0e1fc8a0632d1f0
Hoppy <mbraets@student.42.fr>
2023-05-05 18:01:53 +0200

    Add search bar

--- vite/src/component/chat/ChannelBrowse.tsx
+++ vite/src/component/chat/ChannelBrowse.tsx
@@ -1,15 +1,22 @@
-import { Button, List, ListItem, ListItemText, Theme } from "@mui/material"
+import { Box, Button, List, ListItem, ListItemText, TextField, Theme, Toolbar, Typography } from "@mui/material"
 import { FC, useContext, useEffect, useState } from "react"
 import { Channel } from "../../types"
 import { useNavigate } from "react-router-dom"
 import apiClient from "../../auth/interceptor.axios"
 import { SocketContext } from "../../socket/SocketProvider"
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import { useDebouncedValue } from "../Debounced"
 
 interface ChannelMap { [id: number]: Channel };
 
 export const ChannelBrowser: FC = () => {
 	const [publicChannels, setPublicChannels] = useState<ChannelMap>({});
+	const [searchChannel, setSearchChannel] = useState<string>("");
+	const [debouncedSearchChannel] = useDebouncedValue(searchChannel, 10);
+
 	const navigate = useNavigate();
 
 	const { addSubscription, customOn, customOff } = useContext(SocketContext);
@@ -17,8 +24,6 @@ export const ChannelBrowser: FC = () => {
 	useEffect(() => {
 		return addSubscription(`/chat/public`);
 	}, []);
-
-
 
 	useEffect(() => {
 		function onUpdateChannel(channel: Channel) {
@@ -57,38 +62,99 @@ export const ChannelBrowser: FC = () => {
 	}
 
 	return (
-		<List>
-			{Object.values(publicChannels).map((channel: Channel, index, array) => (
-				<ListItem key={channel.id} sx={{
-					paddingTop: 0,
-					paddingBottom: 0,
-					'&:hover': {
-						bgcolor: (theme: Theme) => theme.palette.grey[200],
-						'> button': {
-							visibility: 'visible'
-						}
-					},
-					borderBottom: array.length - 1 === index ? 1 : 0,
-					borderTop: 1,
-					borderColor: (theme: Theme) => theme.palette.grey[300],
-					'> button': {
-						visibility: 'hidden'
-					}
-				}}>
-					{channel.hasPassword &&
-						<LockOutlinedIcon
-							sx={{
-								color: (theme: Theme) => theme.palette.grey[500],
-								marginRight: 1
-							}}
-						/>}
-					<ListItemText
-						primary={channel.name}
-						secondary={`${channel.members?.length} members`}
+		<Box>
+			<Box sx={{ marginTop: 1 }}>
+				<Search>
+					<SearchIconWrapper>
+						<SearchIcon />
+					</SearchIconWrapper>
+					<StyledInputBase
+						placeholder="Search…"
+						inputProps={{ 'aria-label': 'search' }}
+						value={searchChannel}
+						onChange={(event) => setSearchChannel(event.target.value)}
 					/>
-					<Button onClick={() => joinChannel(channel.id)} variant='contained' color='success'>Join</Button>
-				</ListItem>
-			))}
-		</List>
+				</Search>
+				<Typography variant="subtitle1" sx={{ marginLeft: 1, marginTop: 1 }} color="text.secondary"
+				>{`${Object.values(publicChannels).length} channels`}</Typography>
+			</Box>
+			<List>
+				{Object.values(publicChannels)
+				.filter((channel: Channel) => debouncedSearchChannel.length === 0 || channel.name.toLowerCase().includes(debouncedSearchChannel.toLowerCase()))
+				.map((channel: Channel, index, array) => (
+					<ListItem key={channel.id} sx={{
+						paddingTop: 0,
+						paddingBottom: 0,
+						'&:hover': {
+							bgcolor: (theme: Theme) => theme.palette.grey[200],
+							'> button': {
+								visibility: 'visible'
+							}
+						},
+						borderBottom: array.length - 1 === index ? 1 : 0,
+						borderTop: 1,
+						borderColor: (theme: Theme) => theme.palette.grey[300],
+						'> button': {
+							visibility: 'hidden'
+						}
+					}}>
+						{channel.hasPassword &&
+							<LockOutlinedIcon
+								sx={{
+									color: (theme: Theme) => theme.palette.grey[500],
+									marginRight: 1
+								}}
+							/>}
+						<ListItemText
+							primary={channel.name}
+							secondary={`${channel.members?.length} members`}
+						/>
+						<Button onClick={() => joinChannel(channel.id)} variant='contained' color='success'>Join</Button>
+					</ListItem>
+				))}
+			</List>
+		</Box>
 	)
 }
+
+const Search = styled('div')(({ theme }) => ({
+	position: 'relative',
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: theme.palette.common.white,
+	'&:hover': {
+		backgroundColor: theme.palette.common.white,
+	},
+	marginLeft: 0,
+	width: '100%',
+	[theme.breakpoints.up('sm')]: {
+		marginLeft: theme.spacing(1),
+		width: 'auto',
+	},
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+	padding: theme.spacing(0, 2),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(TextField)(({ theme }) => ({
+	color: 'inherit',
+	'& .MuiInputBase-input': {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			width: '12ch',
+			'&:focus': {
+				width: '20ch',
+			},
+		},
+	},
+}));
