f8cc9b33a24970235495bfe3e5150cb845289ae7
Frederic Le Blay <frederic.leblay@gmail.com>
2023-05-05 13:50:42 +0200

    Maj memberlist en cours

--- vite/src/component/chat/ChannelMemberList.tsx
+++ vite/src/component/chat/ChannelMemberList.tsx
@@ -5,33 +5,46 @@ import { Link as LinkRouter, useNavigate } from "react-router-dom";
 
 import { Channel, Member } from "../../types";
 
+type memberList = {
+	admins: Member[],
+	banned: Member[],
+	muted: Member[],
+	regulars: Member[],
+}
+
+const emptyMemberList: memberList = { admins: [], banned: [], muted: [], regulars: [] }
+
 export function MemberList({ channelId }: { channelId: string }) {
-	const [rows, setRows] = useState<Member[]>([]);
-	const [admin, setAdmin] = useState<Member[]>([]);
-	const [member, setMember] = useState<Member[]>([]);
+	const [memberList, setMemberList] = useState<memberList>(emptyMemberList);
 
 	useEffect(() => {
-		apiClient.get(`/api/chat/channels/${channelId}/members`).then(({data}: {data: Member[]}) => {
+		apiClient.get(`/api/chat/channels/${channelId}/members`).then(({ data }: { data: Member[] }) => {
 			console.log("memberlist fetched : ", data);
-			setRows(data);
+			const newMemberList: memberList = {
+				admins: data.filter((member) => member.role == "admin" || member.role == "owner"),
+				banned: data.filter((member) => member.banned),
+				muted: data.filter((member) => Date.parse(member.muteTime) > Date.now()),
+				regulars: []
+			}
+			newMemberList.regulars = data.filter((member) => {
+				return (!newMemberList.admins.includes(member)
+					&& !newMemberList.banned.includes(member)
+					&& !newMemberList.muted.includes(member))
+			})
+			setMemberList(newMemberList)
 		}).catch((error) => {
 			console.log(error);
 		});
 	}, [channelId]);
 
-	useEffect(() => {
-		setAdmin(rows.filter((row) => row.role === "admin" || row.role === "owner"));
-		setMember(rows.filter((row) => row.role === "regular"));
-	}, [rows]);
-
 	return (
 		<nav aria-label="secondary mailbox folders">
 			<Typography variant="h6" component="div" gutterBottom> Admin </Typography>
 			<List>
-				{admin?.map((admin) => (
+				{memberList.admins.map((admin) => (
 					<ListItem key={admin.id} disablePadding alignItems="flex-start">
 						<ListItemAvatar>
-							<Avatar alt={admin.user.username} src={`/avatars/${admin.id}.png`} style={{width: '15px', height: '15px'}} />
+							<Avatar alt={admin.user.username} src={`/avatars/${admin.id}.png`} style={{ width: '15px', height: '15px' }} />
 						</ListItemAvatar>
 						<ListItemButton >
 							<ListItemText primary={admin.user.username} />
@@ -43,18 +56,17 @@ export function MemberList({ channelId }: { channelId: string }) {
 				)
 				)}
 			</List>
-			<Typography variant="h6" component="div" gutterBottom> Regular </Typography>
 			<List>
-				{member?.map((member) => (
-					<ListItem key={member.id} disablePadding alignItems="flex-start">
+				{memberList.regulars.map((regular) => (
+					<ListItem key={regular.id} disablePadding alignItems="flex-start">
 						<ListItemAvatar>
-							<Avatar alt={member.user.username} src={`/avatars/${member.id}.png`}  style={{width: '15px', height: '15px'}}/>
+							<Avatar alt={regular.user.username} src={`/avatars/${regular.id}.png`} style={{ width: '15px', height: '15px' }} />
 						</ListItemAvatar>
 						<ListItemButton >
-							<ListItemText primary={member.user.username} />
+							<ListItemText primary={regular.user.username} />
 						</ListItemButton>
 						<ListItemAvatar>
-							<Avatar sx={{ bgcolor: member.isConnected ? 'green' : 'red' }} style={{ width: '10px', height: '10px' }} />
+							<Avatar sx={{ bgcolor: regular.isConnected ? 'green' : 'red' }} style={{ width: '10px', height: '10px' }} />
 						</ListItemAvatar>
 					</ListItem>
 				)
