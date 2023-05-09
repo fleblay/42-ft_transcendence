faccdc501deed96230a8c35a60582253e52f61c9
Frederic Le Blay <frederic.leblay@gmail.com>
2023-05-09 10:54:58 +0200

    improved mute modal

--- vite/src/component/chat/MuteMemberModal.tsx
+++ vite/src/component/chat/MuteMemberModal.tsx
@@ -4,15 +4,24 @@ import apiClient from "../../auth/interceptor.axios";
 import { useNavigate } from "react-router-dom";
 import { DatePicker, DateTimePicker } from "@mui/x-date-pickers";
 import { Member } from "../../types";
-import dayjs, {Dayjs} from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 export function MuteMemberModal({ openModal, onClose, channelId, member }: { openModal: boolean, onClose: () => void, channelId: string, member: Member }) {
 
-	const [muteEnd, setMuteEnd] = React.useState<Dayjs | null>(dayjs().add(1, 'hour'));
+	const [muteEnd, setMuteEnd] = React.useState<Dayjs | null>(dayjs().add(1, 'hour').add(1, 'minute'));
 	const mutedState: boolean = Date.parse(member.muteTime) > Date.now()
 
+	const handleChangeValue = (value: Dayjs | null) => {
+		if (!value)
+			return
+		if (value.diff(dayjs()) > 0)
+			setMuteEnd(value)
+		else
+			setMuteEnd(dayjs().add(1, 'hour').add(1, 'minute'))
+	}
+
 	const handleSubmit = () => {
-		apiClient.post(`/api/chat/channels/${channelId}/members/${member.id}`, { mute: mutedState ? dayjs("1970-1-1").toDate(): muteEnd }).
+		apiClient.post(`/api/chat/channels/${channelId}/members/${member.id}`, { mute: mutedState ? dayjs("1970-1-1").toDate() : muteEnd }).
 			then(() => {
 				onClose();
 			})
@@ -39,9 +48,11 @@ export function MuteMemberModal({ openModal, onClose, channelId, member }: { ope
 					<Divider />
 					<form>
 						{
-							!mutedState && <DateTimePicker defaultValue={dayjs().add(1, 'hour')} onChange={(newValue) => setMuteEnd(newValue)} label="Select end of mute period" />
+							!mutedState && <DateTimePicker value={muteEnd} onChange={handleChangeValue} label="Select end of mute period" />
 						}
-						<Button onClick={handleSubmit} variant="outlined" sx={{ flexGrow: 1, mt: '10px', width: '100%', height: '30px' }}>{`${mutedState ? "Unmute" : "Mute"} ${member.user.username}`}</Button>
+						<Button onClick={handleSubmit} variant="outlined" sx={{ flexGrow: 1, mt: '10px', width: '100%', height: '30px' }}>
+							{ mutedState ? "Umute" : `Mute ${member.user.username} for ${muteEnd!.diff(dayjs(), 'hours')} hour(s) and ${muteEnd!.diff(dayjs(), 'minutes') % 60} minute(s)`}
+						</Button>
 						<Divider />
 					</form>
 					<Button onClick={onClose}>Close</Button>
