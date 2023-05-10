584e02b2749153442102d399cc6f389c33680361
jremy <jonathan.prudent13@gmail.com>
2023-05-10 15:00:27 +0200

    [back] cb to emitAll

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -129,12 +129,7 @@ export class ChatService implements OnModuleInit {
 					}
 				});
 				this.wsServer.to(`/chat/myChannels/${addedUser!.id}`).emit('chat.modify.channel', channel);
-				this.emitToAllMembers(channelId, 'chat.modify.channel', async (member: Member) => {
-					const channel = await this.getOneChannel(member.user, channelId);
-					if (!channel)
-						return null;
-					return {...channel, password : undefined, hasPassword : channel.password.length !== 0, members : channel.members.filter((member) => !member.left)};
-				});
+				this.emitToAllMembers(channelId, 'chat.modify.channel', this.cbEmitAll);
 				return
 			}
 		}
@@ -155,12 +150,25 @@ export class ChatService implements OnModuleInit {
 			}
 		});
 		this.wsServer.to(`/chat/myChannels/${addedUser!.id}`).emit('chat.modify.channel', channel);
-		this.emitToAllMembers(channelId, 'chat.modify.channel', async (member: Member) => {
-			const channel = await this.getOneChannel(member.user, channelId);
-			if (!channel)
-				return null;
-			return {...channel, password : undefined, hasPassword : channel.password.length !== 0, members : channel.members.filter((member) => !member.left)};
-		});
+		this.emitToAllMembers(channelId, 'chat.modify.channel', this.cbEmitAll);
+	}
+
+	private async cbEmitAll(member: Member)
+	{
+		const channel = await this.getOneChannel(member.user, member.channel.id);
+		if (!channel)
+			return null;
+		return {
+			...channel,
+			password : undefined,
+			hasPassword : channel.password.length !== 0,
+			members : channel.members.filter((member) => !member.left)
+			.map((member : Member) => (
+				{
+				...member,
+				isConnected: this.usersService.isConnected(member.user.id)
+			}
+			))};
 	}
 
 	private getMemberOfChannel(user: User, channelId: number): Promise<Member | null> {
@@ -391,12 +399,8 @@ export class ChatService implements OnModuleInit {
 				...this.gameService.userState(modifyMember.user.id),
 			}
 		})
-		this.emitToAllMembers(channelId, 'chat.modify.channel', async (member: Member) => {
-			const channel = await this.getOneChannel(member.user, channelId);
-			if (!channel)
-				return null;
-			return {...channel, password : undefined, hasPassword : channel.password.length !== 0, members : channel.members.filter((member) => !member.left)};
-		});			}
+		this.emitToAllMembers(channelId, 'chat.modify.channel', this.cbEmitAll);
+	}
 
 	async modifyChannel(user: User, channelId: number, changeChannelData: ChangeChannelDto) {
 		const requestingMember = await this.getMemberOfChannel(user, channelId);
@@ -426,12 +430,8 @@ export class ChatService implements OnModuleInit {
 			channel.password = changeChannelData.password
 		await this.channelsRepo.save(channel)
 		this.wsServer.to(`/chat/${channelId}`).emit('chat.modify.channel', { channel });
-		this.emitToAllMembers(channelId, 'chat.modify.channel', async (member: Member) => {
-			const channel = await this.getOneChannel(member.user, channelId);
-			if (!channel)
-				return null;
-			return {...channel, password : undefined, hasPassword : channel.password.length !== 0, members : channel.members.filter((member) => !member.left)};
-		});
+		this.emitToAllMembers(channelId, 'chat.modify.channel', this.cbEmitAll);
+
 	}
 
 	async emitToAllMembers(channelId: number, event: string, cb: (member: Member) => any) {
@@ -466,12 +466,8 @@ export class ChatService implements OnModuleInit {
 		const leftMember = await this.membersRepo.save(member);
 		this.wsServer.to(`/chat/${channelId}`).emit('chat.member.leave', { leftMember });
 		this.wsServer.to(`/chat/myChannels/${user.id}`).emit('chat.channel.leave', member.channel.id);
-		this.emitToAllMembers(channelId, 'chat.modify.channel', async (member: Member) => {
-			const channel = await this.getOneChannel(member.user, channelId);
-			if (!channel)
-				return null;
-			return {...channel, password : undefined, hasPassword : channel.password.length !== 0, members : channel.members.filter((member) => !member.left)};
-		});
+		this.emitToAllMembers(channelId, 'chat.modify.channel', this.cbEmitAll);
+
 	}
 
 	async getMyChannels(user: User): Promise<Channel[]> {
