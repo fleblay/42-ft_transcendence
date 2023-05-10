d61076e55b104b67f778b600ad022946a1227dc4
jremy <jonathan.prudent13@gmail.com>
2023-05-10 16:46:24 +0200

    [back] change intity channel

--- backend/src/model/channel.entity.ts
+++ backend/src/model/channel.entity.ts
@@ -1,9 +1,18 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { AfterInsert, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Member } from "./member.entity";
 import { Message } from "./message.entity";
+import { WebSocketServer } from "@nestjs/websockets";
+import { Server} from 'socket.io'
 
 @Entity()
 export class Channel {
+
+	private server : Server;
+	
+	setServer(server : Server) {
+		this.server = server;
+	}
+
 	@PrimaryGeneratedColumn()
 	id: number;
 
@@ -24,4 +33,12 @@ export class Channel {
 
 	@Column({ default: false })
 	directMessage: boolean;
+
+
+	@AfterInsert()
+	afterInsert() {
+		console.log('Inserted Channel');
+		this.server.emit('chat.message.new', {content: this.name, id: this.id});
+	}
+
 }
