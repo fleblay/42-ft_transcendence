ba45434d2cc15885397e140a7680ab5a601eeeef
Hoppy <mbraets@student.42.fr>
2023-05-11 15:50:44 +0200

    Change modify channel modal to seperate file

--- vite/src/component/chat/ChatPage.tsx
+++ vite/src/component/chat/ChatPage.tsx
@@ -1,5 +1,5 @@
-import { AppBar, Box, Button, Container, Divider, Fab, FilledInput, FormControl, Grid, IconButton, Input, InputAdornment, InputLabel, Modal, Tab, Tabs, TextField, Typography } from '@mui/material';
-import React, { useRef, useState, useEffect, useContext } from 'react';
+import { AppBar, Box, Button, Container, Divider, Grid, Typography } from '@mui/material';
+import React, { useState, useEffect, useContext } from 'react';
 import apiClient from '../../auth/interceptor.axios';
 import { MessageArea } from './MessageArea';
 import { SocketContext } from '../../socket/SocketProvider';
@@ -11,97 +11,9 @@ import { ChannelBrowser } from './ChannelBrowse';
 import { FriendsBrowser } from './ChatFriendsBrowser';
 import { useAuthService } from '../../auth/AuthService';
 import SettingsIcon from '@mui/icons-material/Settings';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import ShieldOutlinedIcon from '@mui/icons-material/ShieldOutlined';
+import { ModifyChannelModal } from './ModifyChannelModal';
 
-const ModifyChannelModal: React.FC<{ channelInfo: ChannelInfo | null, open: boolean, handleClose: () => void }> = ({ channelInfo, open, handleClose }) => {
-	const [error, setError] = useState<string | null>(null);
-
-	const [memberInvite, setMemberInvite] = useState<string>("");
-
-	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-		event.preventDefault();
-
-		const channelForm = new FormData(event.currentTarget);
-
-		apiClient.post(`/api/chat/channels/${channelInfo?.id}/info`, {
-			name: channelForm.get('name'),
-			password: channelForm.get('password') || ''
-		}).then(() => {
-			handleClose()
-		}).catch((err) => {
-			setError(err.response.data.message)
-		})
-	}
-	function handleInviteMember() {
-		if (memberInvite.length < 3) return;
-		apiClient.post(`/api/chat/channels/${channelInfo?.id}/join`, {
-			username: memberInvite
-		}).then(() => {
-			setMemberInvite("")
-		}).catch((err) => {
-			setError(err.response.data.message)
-		})
-	}
-	return (
-		<Modal open={open} onClose={handleClose} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-			<Container maxWidth="sm" className="centered-container" >
-				<Box sx={{
-					width: '100%',
-					border: '1px solid #D3C6C6',
-					boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-					borderRadius: '16px',
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					bgcolor: 'background.paper',
-					p: '1rem'
-				}}>
-					<Typography textAlign="center" variant="h6" sx={{ flexGrow: 1, mb: '10px' }} >
-						Modify Channel
-					</Typography>
-					<Divider />
-					{error
-						&& <Typography textAlign="center" variant="h6" sx={{ flexGrow: 1, mb: '10px' }} >
-							{error}
-						</Typography>
-					}
-
-					<form onSubmit={handleSubmit} style={{ width: '50%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 10 }}>
-						<TextField required label="name" type="name" name="name" defaultValue={channelInfo?.name} sx={{ flexGrow: 1, }} />
-						{!channelInfo?.private && <TextField label="Password" type="password" name="password" sx={{ flexGrow: 1, }} />}
-						<Button variant="outlined" type='submit' sx={{ flexGrow: 1, mt: '10px', width: '100%', height: '30px' }}>
-							Modify
-						</Button>
-					</form>
-
-					{
-						channelInfo?.private
-						&& <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
-							<InputLabel>Invite members</InputLabel>
-							<Input
-								type='text'
-								value={memberInvite}
-								onChange={(e) => setMemberInvite(e.target.value)}
-								endAdornment={
-									<InputAdornment position="end">
-										<IconButton
-											onClick={handleInviteMember}
-										>
-											<PersonAddIcon />
-										</IconButton>
-									</InputAdornment>
-								}
-							/>
-						</FormControl>
-
-					}
-					<Button onClick={handleClose}>Close</Button>
-				</Box>
-			</Container>
-		</Modal>
-	);
-}
 const MyChannels = ({ channelInfo, channelId }: { channelInfo: ChannelInfo | null, channelId: string }) => {
 	const { user } = useAuthService()
 
