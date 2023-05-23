5a58557032eb9e2b0d6ae5f488204b10e7d17ead
Hoppy <mbraets@student.42.fr>
2023-05-23 15:26:03 +0200

    Add code / username dto

--- backend/src/users/dtos/create-user.dto.ts
+++ backend/src/users/dtos/create-user.dto.ts
@@ -1,10 +1,13 @@
-import {IsBoolean, IsEmail, IsOptional, IsString} from 'class-validator';
+import {IsBoolean, IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength} from 'class-validator';
 
 export class CreateUserDto{
 	@IsEmail()
 	email: string;
 
 	@IsString()
+	@IsNotEmpty()
+	@MinLength(3)
+	@MaxLength(20)
 	username: string;
 
 	@IsString()
