c8ed9d269e1d30143bc04335302c6d9007255408
jonathan <jonathan.prudent13@gmail.com>
2023-06-02 10:39:32 +0200

    migration to transcendence.jremy.dev

--- envmaker.sh
+++ envmaker.sh
@@ -51,7 +51,7 @@ then
 	sed -i $EXTENSION "s/\(PUBLIC_URL=\).*/\1https:\/\/jremy.dev:443/" .env
 	sed -i $EXTENSION -e "/.*listen 8080.*/{r jremy.conf" -e "d}" ./nginx/nginx.conf
 	sed -i $EXTENSION "s/8080/443/g" ./docker-compose.yml
-	sed -i $EXTENSION "s/\(.*\/nginx\/avatars:\/www\/avatars.*\)/\1\n      - \/etc\/letsencrypt\/archive\/jremy.dev:\/etc\/letsencrypt\/live\/jremy.dev:ro\n/" ./docker-compose.yml
+	sed -i $EXTENSION "s/\(.*\/nginx\/avatars:\/www\/avatars.*\)/\1\n      - \/etc\/letsencrypt\/archive\/transcendence.jremy.dev:\/etc\/letsencrypt\/live\/transcendence.jremy.dev:ro\n/" ./docker-compose.yml
 else
 	echo -e "\x1b[33m42Outside 42paris : HOSTNAME set to localhost to\x1b[0m"
 	HOSTNAME="localhost"
